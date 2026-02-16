7596bb3781bb9b3208a14ec0afb218d3208e53d3
BlubVisInEenPot <mortengoudswaard@gmail.com>
2026-02-16 10:46:39 +0100

    eerse test

--- imagescan.py
+++ imagescan.py
@@ -1,2 +1,9 @@
-# Eerste stat
-print('Hello world!')
+import os
+
+
+list = os.scandir('Y:\Digibende\Amstelveen\Kandidaten\Programmeren\morten')
+for entry in list :
+    if entry.is_dir() or entry.is_file():
+        print(entry.stat().st_ctime)
+        print(entry.name)
+list.close() # Sluiten is netjes om systeem resources te laten vrijmaken
