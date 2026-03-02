a456f1ec059d83d625a3db29fac2c4ab756d681b
BlubVisInEenPot <mortengoudswaard@gmail.com>
2026-03-02 10:40:33 +0100

    alle folders doorzoeken voor alleen fotos en dan info er over printen

--- imagescan.py
+++ imagescan.py
@@ -1,30 +1,45 @@
 import os
 from  datetime import *
 
-# list = os.scandir('Y:\Digibende\Amstelveen\Kandidaten\Programmeren\morten')
-#
-#
-# for entry in list:
-#     d = datetime.fromtimestamp(entry.stat().st_birthtime)
-#
-#     print(d.strftime('%d-%m-%Y')) # 16 feb 2026
+# ik was bij de 7de page van pdf
+
+def isImage(naam):
+    imageExt = [".jpg", ".png", ".jpeg", ".tiff", ".raw", ".dng", ".gif", ]
+    for ext in imageExt:
+        if ext in naam:
+            return True
+    return False
 
 def scanFolders(startFolder):
-    print("Inhoud van" + startFolder)
-    list = os.scandir(startFolder)
-    for entry in list:
-        if entry.is_dir():
+    global found_pictures
 
-            print('[' + entry.name + ']')
+    # print("Inhoud van" + startFolder)
+    list = os.scandir(startFolder)
+
+    for entry in list:
+
+        if entry.is_dir():
+            # print('[' + entry.name + ']')
             scanFolders(os.path.join(startFolder, entry.name))
+
         elif entry.is_file():
-            print(entry.name)
+            if isImage(entry.name):
+                d = datetime.fromtimestamp(entry.stat().st_birthtime)
+
+                print(entry.name)
+                print(d.strftime('%d-%m-%Y'))  # 16 feb 2026
+                print("_________________________________")
+                found_pictures += 1
+
     list.close()
 
+found_pictures = 0
+
 scanFolders('Y:\Digibende\Amstelveen\Kandidaten\Programmeren\morten')
+print("aantal gevonden fotos: " + str(found_pictures))
 
-# print(d)
 
+list = os.scandir('Y:\Digibende\Amstelveen\Kandidaten\Programmeren\morten')
 
 
 
