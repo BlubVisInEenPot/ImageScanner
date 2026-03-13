18647f7277355e732f8206779d4a228a63dcce8f
BlubVisInEenPot <mortengoudswaard@gmail.com>
2026-03-13 14:30:05 +0100

    comments toegevoegt

--- imagescan.py
+++ imagescan.py
@@ -21,7 +21,7 @@ def choosePath():
             break
         else:
             print("path does not exist")
-
+#  ^ vraagt voor een pat die die in DirPath zet
 def chooseSortedPath():
     global sortedPath
 
@@ -36,46 +36,46 @@ def chooseSortedPath():
             break
         else:
             print("path does not exist")
-
+#  ^ vraagt voor een pat waar die de gesoorteerde mapjes met fotos neer zet
 def sortFunc(dictionary):
     return "{:10d}".format(dictionary["size"]) + dictionary["name"].upper()  # RETURN de naam van de gegeven dictionery
-
+#  ^ soorteert de doorgegeven dictionary eerst op naam en dan op maat
 def isImage(naam): # return a True if passed extension name is in the list
     imageExt = [".jpg", ".png", ".jpeg", ".tiff", ".raw", ".dng", ".gif", ]
     for ext in imageExt:
         if ext in naam:
             return True
     return False
+# ^ checkt met doorgegeven naam of een file een foto is
 
-
-def scanFolders(startFolder):
+def scanFolders(startFolder): #
     global found_pictures, imageList
-    bestandData = {}
+    bestandData = {} # maak lege dict aan
 
-    list = os.scandir(startFolder)
+    list = os.scandir(startFolder) # list = start directory
 
-    for entry in list:
+    for entry in list: # ga met entry de heele lijst (directory) langs
 
-        if entry.is_dir():
-            scanFolders(os.path.join(startFolder, entry.name))
+        if entry.is_dir(): # als de entry een directory is
+            scanFolders(os.path.join(startFolder, entry.name)) #zoek dan dieper
 
-        elif entry.is_file():
-            if isImage(entry.name):
+        elif entry.is_file(): # als de entry een file is
+            if isImage(entry.name): # check of entry een imige is met de functie isImage()
 
-                d = datetime.fromtimestamp(entry.stat().st_birthtime)
-                bestandData = { "name": entry.name, "path": entry.path, "size": entry.stat().st_size, "created": entry.stat().st_birthtime }
-                imageList.append(bestandData)
+                d = datetime.fromtimestamp(entry.stat().st_birthtime) # d = de datum ven de entry (de foto dus)
+                bestandData = { "name": entry.name, "path": entry.path, "size": entry.stat().st_size, "created": entry.stat().st_birthtime } # bestandData = een dict met naam,pat,size,dateCreated er in
+                imageList.append(bestandData) # voeg die dict toe aan de imageList nu is de imagelist een lijst met info over de fotos
                 # print(imageList)
                 # print(entry.name)
                 # print(d.strftime('%d-%m-%Y'))  # 16 feb 2026
                 # print("_________________________________")
-                found_pictures += 1
+                found_pictures += 1 # voeg 1 toe aan de gevonden pictures
 
     list.close()
 
+#_______________________________________________________________________________________________________________________________________________
 
 choosePath()
-
 
 scanFolders(DirPath) # roep functie aan
 print("aantal gevonden fotos: " + str(found_pictures)) # print hoe veel fotos er zijn gevenden
@@ -84,10 +84,7 @@ imageList.sort(key=sortFunc) # soorteer de image list
 for i in imageList:# loop door de image list heen
     print(i["name"] + " - " + i["path"] + "  (" + str(i["size"]) + ")")# print de naam van de dictionery waar de loop is
 
-
-
-
-
+print(imageList)
 
 
 
