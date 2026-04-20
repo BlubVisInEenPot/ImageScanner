803c51f0c549c3fed8d9bc714ccea53c1bd4054f
BlubVisInEenPot <mortengoudswaard@gmail.com>
2026-04-20 10:42:15 +0200

    added a paint file for app template and made progres on app with tkinter

--- test.py
+++ test.py
@@ -1,17 +1,28 @@
 from tkinter import *
 from PIL import ImageTk, Image
+from tkinter import filedialog
 
 root = Tk()
-
 root.title("image sorter")
 
-root.geometry("550x300")
 
-root.resizable(width = True, height = True)
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
 
+frame = Frame(root)
+frame.grid(row=0, column=0, sticky="nsew")
 
-btn = Button(root, text="test")
-btn.grid(row = 0, column = 0)
+frame.columnconfigure(0, weight=1)
+frame.rowconfigure(1, weight=1)
+
+entry = Entry(frame)
+entry.grid(row=0, column=0, sticky="ew")
+
+entry_btn = Button(frame, text="button")
+entry_btn.grid(row=0, column=1)
+
+text_list = Listbox(frame)
+text_list.grid(row=1, column=0, columnspan=2, sticky="nsew")
 
 root.mainloop()
 
