0c72ecb8d3283960d7c2cc01120bed8cce9bdf1e
BlubVisInEenPot <m.oogway4304@gmail.com>
2026-05-14 21:57:28 +0200

    changes to test.py

--- test.py
+++ test.py
@@ -3,6 +3,6 @@ import os
 list = os.scandir("/home/OempaLoempa/Pictures/")
 for entry in list :
     if entry.is_dir() or entry.is_file():
-        print(entry.stat().st_birthtime)
+        print(entry.stat().st_ctime)
         print(entry.name)
 list.close() # Sluiten is netjes om systeem resources te laten vrijmaken
