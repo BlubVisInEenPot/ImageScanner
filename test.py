9ce961244f558390fa33de53ca5b3724ef2a7bb1
BlubVisInEenPot <m.oogway4304@gmail.com>
2026-05-16 21:17:21 +0200

    changes to test.py

--- test.py
+++ test.py
@@ -1,17 +1,21 @@
 import os
 
-if os.name == "posix":
-    print("unix")
+def get_creationDate(entry):
+    if os.name == "nt":
+        print("windows")
+        return entry.stat().st_birthtime
+        
+    else:
+        print("unix")
+        return entry.stat().st_ctime
 
-elif os.name == "nt":
-    print("windows")
     
-else:
-    print("?")
 
-# list = os.scandir("/home/OempaLoempa/Pictures/")
-# for entry in list :
-#     if entry.is_dir() or entry.is_file():
-#         print(entry.stat().st_ctime)
-#         print(entry.name)
-# list.close() # Sluiten is netjes om systeem resources te laten vrijmaken
+list = os.scandir("/home/OempaLoempa/Pictures/")
+for entry in list :
+    if entry.is_dir() or entry.is_file():
+        print(get_creationDate(entry))
+        # print(type(entry))
+list.close() # Sluiten is netjes om systeem resources te laten vrijmaken
+
+# print(get_creationDate("/home/OempaLoempa/Pictures/"))
