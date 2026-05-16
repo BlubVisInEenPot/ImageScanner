6c18c48262e352385fbe390a1d9e1ea9b950e9cf
BlubVisInEenPot <m.oogway4304@gmail.com>
2026-05-16 16:28:30 +0200

    changes to test.py

--- test.py
+++ test.py
@@ -1,8 +1,17 @@
 import os
 
-list = os.scandir("/home/OempaLoempa/Pictures/")
-for entry in list :
-    if entry.is_dir() or entry.is_file():
-        print(entry.stat().st_ctime)
-        print(entry.name)
-list.close() # Sluiten is netjes om systeem resources te laten vrijmaken
+if os.name == "posix":
+    print("unix")
+
+elif os.name == "nt":
+    print("windows")
+    
+else:
+    print("?")
+
+# list = os.scandir("/home/OempaLoempa/Pictures/")
+# for entry in list :
+#     if entry.is_dir() or entry.is_file():
+#         print(entry.stat().st_ctime)
+#         print(entry.name)
+# list.close() # Sluiten is netjes om systeem resources te laten vrijmaken
