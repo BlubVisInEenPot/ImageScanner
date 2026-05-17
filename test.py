3ab2141fd5615d9cf0958e8319a89139581fb6bf
BlubVisInEenPot <m.oogway4304@gmail.com>
2026-05-17 12:50:48 +0200

    changes to test.py

--- test.py
+++ test.py
@@ -1,7 +1,27 @@
 import os
 from  datetime import *
+from PIL import Image
+from PIL.ExifTags import TAGS
 
 dateInSec = 0
+
+def get_date_taken(path):
+    # Open the image file
+    img = Image.open(path)
+    
+    # Extract exif data
+    exif_data = img._getexif()
+    
+    if not exif_data:
+        return "No EXIF data found."
+
+    # Search for the 'DateTimeOriginal' tag
+    for tag_id, value in exif_data.items():
+        tag_name = TAGS.get(tag_id, tag_id)
+        if tag_name == 'DateTimeOriginal':
+            return value
+
+    return "Date Taken tag not found."
 
 def get_creationDate(entry):
     # if os.name == "nt": # windows
@@ -13,12 +33,17 @@ def get_creationDate(entry):
         
     # else:               # unix
     #     print("unix")
-    try:
-        print("st_ctime or st_mtime")
-        return min(entry.stat().st_ctime, entry.stat().st_mtime)
-    except:
-        print("st_birthtime")
-        return entry.stat().st_birthtime
+
+    # get_date_taken(entry)
+
+    print("test")
+
+    # try:
+    #     print("st_ctime or st_mtime")
+    #     return min(entry.stat().st_ctime, entry.stat().st_mtime)
+    # except:
+    #     print("st_birthtime")
+    #     return entry.stat().st_birthtime
 
 def convert_time():
     global dateInSec
@@ -26,7 +51,7 @@ def convert_time():
     date = datetime.utcfromtimestamp(dateInSec)
     return date.strftime('%d %b %Y')
 
-list = os.scandir("/home/OempaLoempa/Downloads")
+list = os.scandir(r"C:\Users\vboxuser\Pictures")#\IMG_0332.jpg
 for entry in list :
     if entry.is_dir() or entry.is_file():
         print("_______")
