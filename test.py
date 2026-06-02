4304dcbc4f2b0bb46769e0b0204e33dd8db49dc6
BlubVisInEenPot <mortengoudswaard@gmail.com>
2026-06-02 12:28:23 +0200

    working on error log

--- test.py
+++ test.py
@@ -1,5 +1,14 @@
-string = f"{"hello":15}"
 
-print(string)
-print(len(string))
-print(type(string))
+def clear_log():
+    with open("errors.txt", "wt") as f:
+        f.write("")
+
+def log_errors(exeption):
+    with open("errors.txt", "at") as f:
+        f.write(f"- {exeption}\n\n")
+
+
+clear_log()
+
+for l in range(2):
+    log_errors(f"test{l}")
