2ef60e9a9c56fbab804f7496d4842ded8cd42bbc
BlubVisInEenPot <m.oogway4304@gmail.com>
2026-06-26 23:18:16 +0200

    test commit
    this is a test commit to test github commit on laptop

--- test.py
+++ test.py
@@ -11,9 +11,10 @@ def getExif_data_video(path, data_type):
         return cleanDate
 
     except OSError as e:
-        log_errors(f"getExif_data(): {e}\nwith file: {path}")
+        # log_errors(f"getExif_data(): {e}\nwith file: {path}")
+        print("error")
 
-Date = getExif_data_video(r"\\AMV-DC1.diginet.local\Profiles\morten.goudswaard\videos\file_example_AVI_1920_2_3MG.avi", "encoded_date")
+Date = getExif_data_video(r"C:\Users\mort\Videos\file_example_MP4_1920_18MG.mp4", "encoded_date")
 convertedDate = datetime.strptime(Date, "%Y-%m-%d %H:%M:%S")
 
 print(f"Date: {Date},\nconvertedDate: {convertedDate},\nconvertedDate_type: {type(convertedDate)}")
