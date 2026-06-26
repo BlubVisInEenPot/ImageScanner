b33cfdd1544459d245b250a9f4dfa1e1db92fb22
BlubVisInEenPot <m.oogway4304@gmail.com>
2026-06-27 00:32:22 +0200

    .
    .

--- test.py
+++ test.py
@@ -7,17 +7,20 @@ def getExif_data_video(path, data_type):
         media_info = MediaInfo.parse(path).to_data()
         creation_date = media_info['tracks'][0].get(data_type)
 
-        cleanDate = creation_date.replace("UTC", "").strip()
-        return cleanDate
+        print(creation_date)
+        if creation_date is not None:
+            cleanDate = creation_date.replace("UTC", "").strip()
+            return cleanDate
 
     except OSError as e:
         # log_errors(f"getExif_data(): {e}\nwith file: {path}")
         print("error")
 
-Date = getExif_data_video(r"C:\Users\mort\Videos\file_example_MP4_1920_18MG.mp4", "encoded_date")
-convertedDate = datetime.strptime(Date, "%Y-%m-%d %H:%M:%S")
+Date = getExif_data_video(r"C:\Users\mort\Videos\file_example_WMV_1920_9_3MB.wmv", "encoded_date")
+if Date is not None:
+    convertedDate = datetime.strptime(Date, "%Y-%m-%d %H:%M:%S")
 
-print(f"Date: {Date},\nconvertedDate: {convertedDate},\nconvertedDate_type: {type(convertedDate)}")
+    print(f"Date: {Date},\nconvertedDate: {convertedDate},\nconvertedDate_type: {type(convertedDate)}")
 
 #test commit/puch
 
